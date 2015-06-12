b47960639fce73c166550fd0615144e64a101414
georgschlenkhoff <georg.schlenkhoff@gmail.com>
2015-06-12 08:02:11 +0200

    initial commit

---
+++ specs.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var express=require('express');
+var app=express();
+var bear='Bearer234234ABC';
+app.get('/',function(req,res){
+  res.status(200).send('OK');
+});
+app.get('/login',function(req,res){
+  res.status(200).json({auth:bear});
+});
+app.get('/receive',function(req,res){
+  res.status(200).json({data:'abc'});
+});
+app.post('/send',function(req,res){
+  if(req.headers.authorization!==bear)
+    return res.status(401).json({message:'Unauthorized'});
+  res.status(200).json({message:'OK'});
+});
+app.get('/error',function(req,res){
+  res.status(500).json({message:'Error'});
+});
+app.get('/data',function(req,res){
+  res.status(200).json({items:[{qty:3}]});
+});
+app.listen(3000);
+
+var z=require('./zentest')(app);
+var assert=require('assert');
+
+z.set('Accept','application/json');
+return z.req('get','/',{},{})
+.then(function(r){
+  assert.equal(r,'OK');
+  return z.req('get','/login');
+}).then(function(r){
+  z.auth(r.auth);
+  assert.equal(z._tok,r.auth);
+  return z.req('get','/receive');
+}).then(function(r){
+  z.push('data',r.data);
+  assert.equal(z.get('data'),'abc');
+  z.req('get','/error',{},{expect:500});
+  z.req('get','/data',{},{expect:z.asrt('items[0].qty',3)});
+  z.unauth();
+  z.req('post','/send',{a:'abc'},{expect:401});
+}).done();
---
+++ zentest.js
@@ -0,0 +1,82 @@
+'use strict';
+
+var _=require('lodash');
+var request=require('supertest');
+var Promise=require('promise');
+
+var zentest=function(app){
+  
+  if(!(this instanceof zentest))
+    return new zentest(app);
+  this._app=app;
+};
+
+zentest.prototype={
+
+  asrt:function(prop,val){
+    return function(res){
+      if(_.result(res.body,prop)!==val)
+        throw Error(prop+' should be '+val+' not '+_.result(res.body,prop));
+    };
+  },
+
+  req:function(act,pth,bod,opts){
+    return new Promise(function(resolve){
+      var _req=request(this._app)[act](pth)
+      .expect(200);
+      if(this._header)
+        _.map(this._header,function(obj){_req.set(obj.header,obj.value);});
+      if(this._tok)
+        _req
+        .set('Authorization',this._tok);
+      if(bod)
+        _req.send(bod);
+      opts = (opts) ? opts : [];
+      if((!Array.isArray(opts)))
+        opts=[opts];
+      for(var i=0;i<opts.length;i++){
+        for(var p in opts[i]){
+          if(opts[i].hasOwnProperty(p)){
+            opts[i][p]=(Array.isArray(opts[i][p])) ? opts[i][p] : [opts[i][p]];
+            _req[p].apply(_req,opts[i][p]);
+          }
+        }
+      }
+      _req.end(function(e,r){
+        if(e)
+          throw Error(r.req.method+' '+r.req.path+': '+e);
+        else{
+          if (/application\/json/.test(r.res.headers['content-type']))
+            resolve(JSON.parse(r.text));
+          else
+            resolve(r.text);
+        }
+      });
+    }.bind(this));
+  },
+
+  set:function(header,value){
+    this._header=[];
+    this._header.push({header:header,value:value});
+  },
+
+  auth:function(tok){
+    this._tok=tok;
+  },
+
+  unauth:function(){
+    this._tok=undefined;
+  },
+
+  push:function(key,val){
+    this.dict=(this.dict) ? this.dict : {};
+    this.dict[key]=val;
+  },
+
+  get:function(key){
+    return this.dict[key];
+  },
+
+};
+
+module.exports=zentest;
